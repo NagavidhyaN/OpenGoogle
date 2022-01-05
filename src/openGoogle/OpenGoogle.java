560a476223935bebf129f7fffeabd856fad400a7
Home <Home@DESKTOP-I266SV3>
2022-01-05 10:48:57 +0530

    Initial commit

---
+++ src/openGoogle/OpenGoogle.java
@@ -0,0 +1,12 @@
+package openGoogle;
+
+public class OpenGoogle {
+
+	public static void main(String[] args) {
+		System.out.println("Open Url");
+		System.out.println("Open Google Page");
+		System.out.println("Type What You Want and Search");
+
+	}
+
+}
